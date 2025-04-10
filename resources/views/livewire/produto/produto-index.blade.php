e95b1042fb96ff1f47ee8ee73a7a4e572a0135cf
richardPilonn <richard.pilonzera@gmail.com>
2025-04-10 12:52:36 -0300

    Implementado Model, Migrate, Index, e seed do cru de Produtos

--- resources/views/livewire/produto/produto-index.blade.php
+++ resources/views/livewire/produto/produto-index.blade.php
@@ -1,20 +1,21 @@
 <div class="container mt-4">
     <div class="row mb-3">
         <div class="col-md-6">
-            <h2>Clientes</h2>
+            <h2>Produtos</h2>
         </div>
         <div class="col-md-6 text-end">
-            <a href="{{ route('clientes.create') }}" class="btn btn-primary">
-                <i class="bi bi-plus-circle"></i> Novo Cliente
+            <a href="{{ route('produtos.create') }}" class="btn btn-primary">
+                <i class="bi bi-plus-circle"></i> Novo Produto
             </a>
         </div>
     </div>
+
     <div class="card">
         <div class="card-body">
             <div class="row mb-3">
                 <div class="col-md-6">
-                    <input type="text" wire:model.debounce.300ms="search" class="form-control"
-                        placeholder="Buscar Clientes...">
+                    <input type="text" wire:model.debounce.300ms="search" 
+                    class="form-control" placeholder="Buscar produtos...">
                 </div>
                 <div class="col-md-3">
                     <select wire:model="perPage" class="form-select">
@@ -25,8 +26,9 @@
                     </select>
                 </div>
             </div>
-            @if (session()->has('message'))
-                <div class="alert alert-sucess">
+
+            @if(session()->has('message'))
+                <div class="alert alert-success">
                     {{ session('message') }}
                 </div>
             @endif
@@ -36,42 +38,45 @@
                     <thead>
                         <tr>
                             <th>Nome</th>
-                            <th>CPF</th>
-                            <th>Email</th>
-                            <th>Telefone</th>
+                            <th>Ingredientes</th>
+                            <th>Valor</th>
                             <th>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @forelse($clientes as $cliente)
+                        @forelse($produtos as $produto)
                             <tr>
-                                <td>{{ $cliente->nome }}</td>
-                                <td>{{ $cliente->cpf }}</td>
-                                <td>{{ $cliente->email }}</td>
-                                <td>{{ $cliente->telefone }}</td>
+                                <td>{{ $produto->nome }}</td>
+                                <td>{{ $produto->igredientes }}</td>
+                                <td>{{ $produto->valor }}</td>
                                 <td>
-                                    <a href="{{ route('clientes.show', $cliente->id) }}" class="btn btn-sm btn-info">
+                                    <a href="{{ route('produtos.show', $produto->id) }}" 
+                                        class="btn btn-sm btn-info">
                                         <i class="bi bi-eye"></i>
                                     </a>
-                                    <a href="{{ route('clientes.edit', $cliente->id) }}" class="btn btn-sm btn-warning">
+                                    <a href="{{ route('produtos.edit', $produto->id) }}" 
+                                        class="btn btn-sm btn-warning">
                                         <i class="bi bi-pencil"></i>
                                     </a>
-                                    <button wire:click="delete ({{ $cliente->id }})" class="btn btn-sm btn-danger"
-                                        onclick="return confirm('Tem certeza?')">
+                                    <button wire:click="delete({{ $produto->id }})" 
+                                        class="btn btn-sm btn-danger" onclick="return 
+                                        confirm('Tem certeza?')">
                                         <i class="bi bi-trash"></i>
                                     </button>
                                 </td>
                             </tr>
                         @empty
-                        <tr>
-                            <td colspan="5" class="text-center">Nenhum Cliente encontrado.</td>
-                        </tr>
+                            <tr>
+                                <td colspan="5" class="text-center">
+                                    Nenhum produto encontrado.</td>
+                            </tr>
                         @endforelse
                     </tbody>
                 </table>
             </div>
+
             <div class="mt-3">
-                {{ $clientes->links() }}
+                {{ $produtos->links() }}
             </div>
         </div>
     </div>
