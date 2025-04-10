fe85bf28472a5e26b7049c4e2bcce19b279b17ac
richardPilonn <richard.pilonzera@gmail.com>
2025-04-10 10:33:46 -0300

    Implementado Model, Migrate, Index, e seed do cru de Clientes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('clientes')->group(function () {
+    Route::get('/', \App\Livewire\Clientes\Index::class)->name('clientes.index');
+    Route::get('/create', \App\Livewire\Clientes\Create::class)->name('clientes.create');
+    Route::get('/{clientes}', \App\Livewire\Clientes\Show::class)->name('clientes.show');
+    Route::get('/{clienes}/edit', \App\Livewire\Clientes\Edit::class)->name('clientes.edit');
 });
